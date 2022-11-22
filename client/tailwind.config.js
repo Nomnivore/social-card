a8221b4a91e113d7d8ba7301fc8642841bfe89ec
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-22 17:38:34 -0500

    install tailwind and daisyui

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
