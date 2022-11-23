39e385b8b67479185b92f34380b8d92a74444bad
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-22 21:47:47 -0500

    add db models

---
+++ server/Models/Link.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Link
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public LinkTypeEnum LinkType { get; set; }
+    }
+
+    public enum LinkTypeEnum
+    {
+        YouTube,
+        GitHub,
+        LinkedIn,
+        Twitter
+    }
+}
---
+++ server/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace server.Models
+{
+    public class User : IdentityUser
+    {
+        // id, username, password, and email are already
+        // implemented by IdentityUser
+        // https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.identityuser?view=aspnetcore-6.0
+
+        public List<Link> Links { get; set; }
+
+    }
+}
