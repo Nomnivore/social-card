5335eeeb2c9fc876f59cc85121f722031d7b2c1b
Doug <70054506+smykad@users.noreply.github.com>
2022-11-24 06:00:12 -0500

    formatting files

--- client/src/pages/Directory.js
+++ client/src/pages/Directory.js
@@ -1,27 +1,32 @@
 import Card from "../components/Card";
 
 const Directory = () => {
-    const data = [
-        {
-            cardInitials: 'FF', userName: '@FredFlintstone', myLinks: ["Twitter/FredF", "Linkedin/FredF", "Youtube/FredF", "Github/FredF"]
-        }
-    ];
+  const data = [
+    {
+      cardInitials: "FF",
+      userName: "@FredFlintstone",
+      myLinks: [
+        "Twitter/FredF",
+        "Linkedin/FredF",
+        "Youtube/FredF",
+        "Github/FredF",
+      ],
+    },
+  ];
 
-    return (
-
-        
+  return (
     <div>
       <h1 className="text-5xl flex justify-center p-5">Directory</h1>
       <div className="flex justify-evenly">
-          <Card
-            cardInitials={data[0].cardInitials}
-            userName={data[0].userName}
-            myLinks={[
-                data[0].myLinks[0],
-                data[0].myLinks[1],
-                data[0].myLinks[2],
-                data[0].myLinks[3]
-            ]}
+        <Card
+          cardInitials={data[0].cardInitials}
+          userName={data[0].userName}
+          myLinks={[
+            data[0].myLinks[0],
+            data[0].myLinks[1],
+            data[0].myLinks[2],
+            data[0].myLinks[3],
+          ]}
         />
         <Card
           cardInitials="BR"
