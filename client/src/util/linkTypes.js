6ed8aa001b0cee07a0ae768d4d957c2845294bdf
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-29 12:54:04 -0500

    add utils

---
+++ client/src/util/linkTypes.js
@@ -0,0 +1,20 @@
+const linkTypes = [
+  {
+    name: "YouTube",
+    icon: null,
+  },
+  {
+    name: "GitHub",
+    icon: null,
+  },
+  {
+    name: "Linkedin",
+    icon: null,
+  },
+  {
+    name: "Twitter",
+    icon: null,
+  },
+];
+
+export default linkTypes;
