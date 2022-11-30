b833706ee785c5a60849bb256e497b74eadccbd7
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-30 14:25:04 -0500

    add viewmodels

---
+++ server/ViewModels/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace server.ViewModels
+{
+    public class LoginResponse : Response
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public string Username { get; set; }
+    }
+}
