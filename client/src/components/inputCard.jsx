035a3613c851f40e7fa3a1c47e3aa6a06074f698
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-12-01 02:27:08 -0500

    register form wiring

--- client/src/components/inputCard.jsx
+++ client/src/components/inputCard.jsx
@@ -2,7 +2,14 @@ import PropTypes from "prop-types";
 import Input from "./Input";
 import { Link } from "react-router-dom";
 
-function InputCard({ inputTitle, inputArray, buttonName, pageName, linkName }) {
+function InputCard({
+  inputTitle,
+  inputArray,
+  buttonName,
+  pageName,
+  linkName,
+  buttonClick,
+}) {
   return (
     <div className="card w-96 bg-base-200 shadow-xl">
       <div className="card-body">
@@ -13,11 +20,14 @@ function InputCard({ inputTitle, inputArray, buttonName, pageName, linkName }) {
               inputLabel={object.inputLabel}
               placeholder={object.placeholder}
               key={i}
+              change={object.change}
             />
           ))}
         </div>
         <div className="card-actions flex justify-end">
-          <button className="btn btn-primary">{buttonName}</button>
+          <button className="btn btn-primary" onClick={buttonClick}>
+            {buttonName}
+          </button>
         </div>
         <div className="card-actions justify-center">
           <Link to={pageName} className="link link-primary">
@@ -28,11 +38,19 @@ function InputCard({ inputTitle, inputArray, buttonName, pageName, linkName }) {
     </div>
   );
 }
+
+const inputPropTypes = PropTypes.shape({
+  inputLabel: PropTypes.string,
+  placeholder: PropTypes.string,
+  change: PropTypes.func,
+});
+
 InputCard.propTypes = {
   inputTitle: PropTypes.string,
-  inputArray: PropTypes.array,
+  inputArray: PropTypes.arrayOf(inputPropTypes),
   buttonName: PropTypes.string,
   pageName: PropTypes.string,
   linkName: PropTypes.string,
+  buttonClick: PropTypes.func,
 };
 export default InputCard;
