d3391a66d2157fa53741aed193c800f8a7a27f3b
smykad <dougsmyka@gmail.com>
2022-12-06 22:49:52 -0500

    add-link, editor-card

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/directory" element={<Directory />} />
-        <Route path="/editor" element={<Editor />}/>
+        <Route path="/editor" element={<Editor />} />
         <Route path="/@:username" element={<Details />} />
 
         <Route element={<GuestRoute />}>
