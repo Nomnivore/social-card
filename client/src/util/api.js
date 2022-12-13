48b74e67110f887278d9dd86e1d3f5ccffd2cf60
nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-12-13 15:38:45 -0500

    prepare for deployment

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
+let baseURL = "/api";
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV == "production")
+  baseURL = `http://warne157-001-site1.atempurl.com${baseURL}`;
+
 export default axios.create({
-  baseURL: "/api",
+  baseURL: baseURL,
   headers: { "Content-Type": "application/json" },
 });
 
 export const axiosToken = axios.create({
-  baseURL: "/api",
+  baseURL: baseURL,
   headers: { "Content-Type": "application/json" },
 });
